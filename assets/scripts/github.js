6248d8d391a29b28fb1cb191d886e79b38b2cf73
Natan Augusto <natanaugusto@gmail.com>
2021-11-17 22:33:39 -0300

    Added insert github data (Just for users offcourse)
    I'm little tired of play with WordPress for now

--- assets/scripts/github.js
+++ assets/scripts/github.js
@@ -3,6 +3,7 @@
  * But I'm lazy
  */
 jQuery(document).ready(() => {
+	let results;
 	let timeout = null;
 
 	/**
@@ -21,45 +22,61 @@ jQuery(document).ready(() => {
 	 * @param {callback} callback
 	 * @param {object} settings
 	 */
-	const sendRequestSearch = (callback, settings) => {
+	const sendRequest = (callback, settings) => {
 		settings._ajax_nonce = github_analyzer_ajax.nonce;
 		jQuery.post(github_analyzer_ajax.ajax_url, settings, callback);
+	};
+
+	const addCallback = (data) => {
+		console.log(data);
+	};
+
+	/**
+	 * The callback for the search from GitHub API return
+	 * @param {object} data
+	 */
+	const searchCallback = (data) => {
+		if (data.code !== 200 || data.body.total_count < 1) {
+			jQuery(".github-analizer-block .results").hide();
+			return;
+		}
+		jQuery(".github-analizer-block .results").show();
+		jQuery(".github-analizer-block .results ul li").remove();
+		jQuery(
+			".github-analizer-block .results div.header span.total span.value"
+		).text(0);
+		jQuery(
+			".github-analizer-block .results div.header span.total span.value"
+		).text(data.body.total_count);
+		results = data.body.items;
+		results.forEach((item, index) => {
+			jQuery(".github-analizer-block .results ul").append(
+				`<li key="${index}">
+					<img src="${item.avatar_url}" />
+					${item.login}
+				</li>`
+			);
+		});
+		jQuery(".github-analizer-block .results ul li").click((event) => {
+			let key = jQuery(event.target).attr("key");
+			sendRequest(addCallback, {
+				type: "user",
+				data: results[key],
+				action: "github_analyzer_add",
+			});
+		});
 	};
 
 	/**
 	 * GitHub Search
 	 */
-	jQuery("#github-search").keyup((el) => {
+	jQuery("#github-search").keyup((event) => {
 		singleSetTimeout(function () {
-			sendRequestSearch(
-				(data) => {
-					if (data.code !== 200 || data.body.total_count < 1) {
-						jQuery(".github-analizer-block .results").hide();
-						return;
-					}
-					jQuery(".github-analizer-block .results").show();
-					jQuery(".github-analizer-block .results ul li").remove();
-					jQuery(
-						".github-analizer-block .results div.header span.total span.value"
-					).text(0);
-					jQuery(
-						".github-analizer-block .results div.header span.total span.value"
-					).text(data.body.total_count);
-					data.body.items.forEach((item) => {
-						jQuery(".github-analizer-block .results ul").append(
-							`<li>
-								<img src="${item.avatar_url}" />
-								${item.login}
-							</li>`
-						);
-					});
-				},
-				{
-					type: "users",
-					search: el.target.value,
-					action: "github_analyzer_search",
-				}
-			);
+			sendRequest(searchCallback, {
+				type: "users",
+				search: event.target.value,
+				action: "github_analyzer_search",
+			});
 		});
 	});
 });
--- src/GithubAnalyzer.php
+++ src/GithubAnalyzer.php
@@ -3,9 +3,9 @@ namespace GithubAnalyzer;
 
 class GithubAnalyzer extends GithubAPI
 {
-	/**
-	 * @var GithubAPI $api
-	 */
+    /**
+     * @var GithubAPI $api
+     */
     protected $api;
     /**
      * @var string $handle_name
@@ -14,9 +14,14 @@ class GithubAnalyzer extends GithubAPI
 
     public function __construct()
     {
+        $indexFile = plugin_dir_path(__DIR__) . 'index.php';
+        register_activation_hook($indexFile, [$this, 'create_database_table']);
+        register_deactivation_hook($indexFile, [$this, 'drop_database_table']);
+
         add_action('plugins_loaded', [$this, 'set_credentials']);
         add_action('wp_enqueue_scripts', [$this, 'enqueue_assets']);
         add_action("wp_ajax_{$this->handle_name}_search", [$this, 'ajax_search_handler']);
+        add_action("wp_ajax_{$this->handle_name}_add", [$this, 'ajax_add_handler']);
         add_action("wp_ajax_nopriv_{$this->handle_name}_search", [$this, 'ajax_search_handler']);
         add_action('enqueue_block_editor_assets', [$this, 'enqueue_block_editor_assets']);
 
@@ -38,7 +43,7 @@ class GithubAnalyzer extends GithubAPI
         );
     }
 
-	/**
+    /**
      * Extend the timeout since API calls can easily exceed 5 seconds
      *
      * @param int $seconds The current timeout setting
@@ -106,16 +111,96 @@ class GithubAnalyzer extends GithubAPI
         );
     }
 
-	/**
-	 * Handle the search from GitHubAPI
-	 *
-	 * @return void
-	 */
+    /**
+     * Handle the search from GitHubAPI
+     *
+     * @return void
+     */
     public function ajax_search_handler(): void
     {
         check_ajax_referer($this->handle_name);
 
-		// Mysterious, isn't it?
+        // Mysterious, isn't it?
         wp_send_json($this->api->{$_POST['type']}($_POST['search']));
+    }
+
+    /**
+     * Add a new data from a github response
+     *
+     * @return void
+     */
+    public function ajax_add_handler(): void
+    {
+        check_ajax_referer($this->handle_name);
+        global $wpdb;
+        $table = $this->table_name($wpdb);
+        # TODO: Insert/Update method
+        $wpdb->insert(
+            $table,
+            [
+                'id' => (int) $_POST['data']['id'],
+                'type' => $_POST['type'],
+                'data' => json_encode($_POST['data']),
+            ],
+            ['%d', '%s', '%s']
+        ) ? wp_send_json(null, 201) : wp_send_json(["message" => "Error"], 500);
+
+    }
+
+    /**
+     * The method does what they name say
+     *
+     * @return void
+     */
+    public function drop_database_table(): void
+    {
+        $this->run_query("DROP TABLE IF EXISTS :table");
+    }
+
+    /**
+     * The method does what they name say
+     *
+     * @return void
+     */
+
+    public function create_database_table(): void
+    {
+        $this->run_query(<<<SQL
+		CREATE TABLE IF NOT EXISTS :table (
+			`id` INT NOT NULL,
+			`type` enum('user', 'repository') default 'user',
+			`data` text DEFAULT NULL,
+			PRIMARY KEY(`id`)
+		)
+		SQL);
+    }
+
+    /**
+     * Execute an CREATE/DROP query in database
+     *
+     * @param string $sql
+     *
+     * @return void
+     */
+    private function run_query(string $sql): void
+    {
+        global $wpdb;
+        $table = $this->table_name($wpdb);
+        $sql = str_replace(':table', $table, $sql);
+        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+        dbDelta($sql);
+
+    }
+
+    /**
+     * Get the plugin table name
+     *
+     * @param object $wpdb
+     *
+     * @return string
+     */
+    private function table_name(object $wpdb): string
+    {
+        return "{$wpdb->prefix}{$this->handle_name}_data";
     }
 }
